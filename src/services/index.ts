746d385f361baf71be5817c9110c7a3832e4b3f7
MattGoodwinSlackLine <mttgoodwin55+Stackline@gmail.com>
2023-12-04 01:51:04 -0800

    Updates

--- src/services/index.ts
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import stackline_data from "../assets/stackline_data.json";
-
-export const fetchContent = createAsyncThunk("app/fetchContent", async () => {
-  //Fake Fetch Call
-  const data = await stackline_data[0];
-  return data;
-});
